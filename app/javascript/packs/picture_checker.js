c6dbc8bfaa95fccdc9bd3b54e2df22b3cdad6475
Hleb5364 <borusov5364@gmail.com>
2020-09-20 00:20:41 +0300

    Compare images

--- app/javascript/packs/picture_checker.js
+++ app/javascript/packs/picture_checker.js
@@ -1,13 +1,40 @@
 $(function(){
   $('body').on('change', '[data-image-file]', function(){
-    var imageExtension = $('[data-image-file]')[0].files[0].name.split(".")[1]
-    var similarImageExtension = $("[data-main-image]")[0].src.split(".")[1]
-    debugger
-    if (imageExtension !=  similarImageExtension) {
-      $('[data-image-file]').val(null)
-      $('[data-image-label]').text("Додати схожу картинку до галереї")
-      alert("Розширення картинки має бути " + imageExtension)
-    }
-    
+    compareImages(this)
+    // var imageExtension = $('[data-image-file]')[0].files[0].name.split(".")[1]
+    // var similarImageExtension = $("[data-main-image]")[0].src.split(".")[1]
+    // if (imageExtension !=  similarImageExtension) {
+    //   compareImagesSize(this)
+    //   $('[data-image-file]').val(null)
+    //   $('[data-image-label]').text("Додати схожу картинку до галереї")
+    //   alert("Розширення картинки має бути " + imageExtension)
+    // }
   })
 })
+
+function compareImages(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      var img = new Image;
+      img.onload = function() {
+        if(img.width != $("img").get(0).naturalWidth && img.height != $("img").get(0).naturalHeight){
+          alert("Розміри картинки мають бути " + $("img").get(0).naturalWidth + "x" + $("img").get(0).naturalHeight)
+          compareExtension()
+        }
+      };
+      img.src = reader.result;
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+function compareExtension(){
+  var imageExtension = $('[data-image-file]')[0].files[0].name.split(".")[1]
+  var similarImageExtension = $("[data-main-image]")[0].src.split(".")[1]
+  if (imageExtension !=  similarImageExtension) {
+    $('[data-image-file]').val(null)
+    $('[data-image-label]').text("Додати схожу картинку до галереї")
+    alert("Розширення картинки має бути " + imageExtension)
+  }
+}

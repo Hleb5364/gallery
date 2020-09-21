52d626022461aed798d723a4f12c304633c7bbd4
Hleb5364 <borusov5364@gmail.com>
2020-09-21 22:39:03 +0300

    Smale fix

--- app/models/picture_gallery_image.rb
+++ app/models/picture_gallery_image.rb
@@ -27,16 +27,24 @@ class PictureGalleryImage < ApplicationRecord
     original_image = MiniMagick::Image.open(image)
     images.each do |img|
       similar_image = MiniMagick::Image.open(img.tempfile.to_path)
-      unless original_image.width == similar_image.width && original_image.height == similar_image.height
+      unless compareWidth(similar_image.width, original_image.width) && compareHeight(similar_image.height, original_image.height)
         errors.add(:similar_images, "Формат зображень має бути #{original_image[:width]}x#{original_image[:height]}")
       end
     end
   end
 
+  def compareWidth(similarImgWidth, originalImgWidth)
+    (originalImgWidth + originalImgWidth * 0.1) >= similarImgWidth && (originalImgWidth - originalImgWidth * 0.1) <= similarImgWidth
+  end
+
+  def compareHeight(similarImgHeight, originalImgHeight)
+    (originalImgHeight + originalImgHeight * 0.1) >= similarImgHeight && (originalImgHeight - originalImgHeight * 0.1) <= similarImgHeight
+  end
+
   def check_format(images)
     images.each do |img|
       image_file = MiniMagick::Image.open(img.tempfile.to_path)
-      unless image_file.type.downcase == image.filename.extension
+      unless image_file.path.split(".").last == image.filename.extension
         errors.add(:similar_images, "Формат зображень має бути #{image.filename.extension}")
       end
     end
